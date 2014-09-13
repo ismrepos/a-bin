499ba064c616c45a1cf9865f89fc66ec972bc2b6
keisukehonda <keisukeh@gmail.com>
2014-09-13 14:46:17 +0900

    modify deploy-ttmi.sh

---
+++ deploy-ttmi.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+SERVER=ura1
+USER=khonda
+TTMI_HOME=/home/$USER/Projects/
+TAR_FILE=hoge.tar.gz
+TMP_DIR=/home/$USER/tmp
+DEPLOY_HOME=~/
+echo Deoloying TTMI Project from $SERVER
+
+echo Packing TTMI Project
+cat << EOF > /tmp/CMD.txt
+cd $TTMI_HOME
+tar zcvf $TMP_DIR/$TAR_FILE . --exclude .git*
+EOF
+CMD=`cat /tmp/CMD.txt`
+rm /tmp/CMD.txt
+ssh $USER@$SERVER "$CMD"
+
+echo Copy TTMI Project from $SERVER
+scp $USER@$SERVER:$TMP_DIR/$TAR_FILE .
+
+echo Delete $TAR_FILE from $SERVER
+cat << EOF > /tmp/CMD.txt
+rm $TMP_DIR/$TAR_FILE
+EOF
+CMD=`cat /tmp/CMD.txt`
+rm /tmp/CMD.txt
+ssh $USER@$SERVER "$CMD"
+
+echo Unpacking TTMI Project
+tar zxvf ./$TAR_FILE -C $DEPLOY_HOME
+rm ./$TAR_FILE
