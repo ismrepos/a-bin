5cbb466c92e547d3cd92ef3781c527c544b3318a
keisukehonda <keisukeh@gmail.com>
2014-09-15 22:05:03 +0900

    modify

--- batch-ttmi.sh
+++ batch-ttmi.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+mkdir ~/ttmi/out
+
 #PBS -q uv2k-s
 #PBS -N TEST_JOB
 #PBS -l select=1:ncpus=1:mem=1gb
-#PBS -o out_file
+#PBS -o ~/ttmi/out/out_file
 #PBS -j oe
 
-cd /home1/khonda/ttmi
+cd ~/ttmi
 dplace ./bin/run.sh
 
 
