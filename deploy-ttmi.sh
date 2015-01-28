034393ec4ad3684701b1ab16558dbbb10aa911d8
keisukehonda <keisukeh@gmail.com>
2015-01-28 15:23:06 +0900

    modify

--- deploy-ttmi.sh
+++ deploy-ttmi.sh
@@ -9,20 +9,25 @@ TMP_DIR=/home/$USER/tmp
 DEPLOY_HOME=~/
 echo Deoloying TTMI Project from $SERVER
 
-echo Packing TTMI Project
+case $1 in
+  csv)
+    echo Packing CSV files
+    cat << EOF > /tmp/CMD.txt
+    cd $TTMI_DIR
+    tar zcvf $TMP_DIR/$TAR_FILE --exclude .git* $TTMI_HOME/csv
+    EOF
+    ;;
+  batch)
+    echo Packing batch files
+    ;;
+  *)
+    echo Packing TTMI Project
+    cat << EOF > /tmp/CMD.txt
+    cd $TTMI_DIR
+    tar zcvf $TMP_DIR/$TAR_FILE -X $TTMI_HOME/.tarignore $TTMI_HOME
+    EOF
+  ;;
 
-if [ "$1" != "csv" ]
-then
-cat << EOF > /tmp/CMD.txt
-cd $TTMI_DIR
-tar zcvf $TMP_DIR/$TAR_FILE -X $TTMI_HOME/.tarignore $TTMI_HOME
-EOF
-else
-cat << EOF > /tmp/CMD.txt
-cd $TTMI_DIR
-tar zcvf $TMP_DIR/$TAR_FILE --exclude .git* $TTMI_HOME/csv
-EOF
-fi
 CMD=`cat /tmp/CMD.txt`
 rm /tmp/CMD.txt
 ssh $USER@$SERVER "$CMD"
@@ -38,6 +43,6 @@ CMD=`cat /tmp/CMD.txt`
 rm /tmp/CMD.txt
 ssh $USER@$SERVER "$CMD"
 
-echo Unpacking TTMI Project
+echo Unpacking
 tar zxvf ./$TAR_FILE -C $DEPLOY_HOME
 rm ./$TAR_FILE
