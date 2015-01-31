6c72429592a256ab86a655c8009adab282b834f0
keisukehonda <keisukeh@gmail.com>
2015-01-31 15:19:58 +0900

    mid queue

--- batch-omp.sh
+++ batch-omp.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-#PBS -q uv2k-s
+#PBS -q uv2k-m
 #PBS -N TEST_JOB
-#PBS -l select=1:ncpus=1:mem=1gb
+#PBS -l select=1:ncpus=12:mem=12gb
 #PBS -o out_file
 #PBS -j oe
+export OMP_NUM_THREADS=6
+export KMP_AFFINITY=disalbed
 
 cd ~/ttmi/src
-dplace ./ttmi input_file_origin.txt
-
+dplace -X ./ttmi input_file_origin.txt > test.log
